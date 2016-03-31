d7d81baa024495a1581a9e05b2164ab7006c6c4f
Echo13243 <timothyvu2002@gmail.com>
2016-03-30 20:18:20 -0400

    Added files via upload

---
+++ conf.lua
@@ -0,0 +1,8 @@
+function love.conf(t)
+
+  t.title = "DECISION" --Title of window
+  t.version = "0.10.1" --Version of LOVE
+  t.window.width = 999  --Window dimensions
+  t.window.height = 399 --Window dimensions
+
+end
